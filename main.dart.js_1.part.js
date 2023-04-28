d7aca9cdad358db58c3b43fe8845d1146c5331e8
Daniel <daniel.huser@sap.com>
2023-04-28 09:09:19 +0000

    update

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,40 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={kY:function kY(){},
+aPb(){B.wr("en")
+return new A.oV()},
+oV:function oV(){}},B
+A=a.updateHolder(c[3],A)
+B=c[0]
+A.kY.prototype={}
+A.oV.prototype={
+Wu(d){return d+" GitHub repository"},
+aeU(d){return"To see the source code for this app, please visit the "+d+"."},
+gXk(){return"Settings"},
+gIZ(){return"Settings"},
+gIY(){return"Close settings"},
+gAO(){return"System"},
+gXe(){return"Text scaling"},
+gXi(){return"Small"},
+gXh(){return"Normal"},
+gXg(){return"Large"},
+gXf(){return"Huge"},
+gXa(){return"Text direction"},
+gXc(){return"Based on locale"},
+gXb(){return"LTR"},
+gXd(){return"RTL"},
+gX6(){return"Locale"},
+gX7(){return"Platform mechanics"},
+gXj(){return"Theme"},
+gX3(){return"Dark"},
+gX5(){return"Light"},
+gX9(){return"Slow motion"},
+gX1(){return"About Goomba"},
+gX4(){return"Send feedback"},
+gX2(){return"build with love in switzerland"},
+gUD(){return"Privacy"},
+gX8(){return"Privacy Policy"}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.kY,B.J)
+y(A.oV,A.kY)})()
+B.aGe(b.typeUniverse,JSON.parse('{"oV":{"kY":[]}}'))}
+$__dart_deferred_initializers__["M3ttO3Ba2zB9dDsDvwGKtH0V9JU="] = $__dart_deferred_initializers__.current
